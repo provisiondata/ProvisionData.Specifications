2c7d84a7d8d330f5ec0f27d559369edfd6154b77
Doug Wilson <doug@provisiondata.com>
2020-01-16 09:54:11 -0800

    Added StringExtentions.Left(input, length)

---
+++ tests/ProvisionData.Common.UnitTests/Extensions/StringExtentions_Left.cs
@@ -0,0 +1,21 @@
+﻿namespace ProvisionData.Extensions
+{
+    using System;
+    using FluentAssertions;
+    using Xunit;
+
+    public class StringExtentions_Left
+    {
+        [Theory]
+        [InlineData("", 0, "")]
+        [InlineData("", 5, "")]
+        [InlineData("  ", 0, "")]
+        [InlineData("  ", 5, "")]
+        [InlineData("Hello", 0, "")]
+        [InlineData("Hello", 1, "H")]
+        [InlineData("Hello", 5, "Hello")]
+        [InlineData("Hello", 6, "Hello")]
+        public void Should_return_the_correct_result(String input, Int32 length, String expected)
+           => input.Left(length).Should().Be(expected);
+    }
+}
