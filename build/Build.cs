0696d1faf026b160361e47036c71ecf905a3a640
Doug Wilson <d@dkw.io>
2021-08-26 18:54:28 -0700

    Upgrade Nuke.Build - Part 2

--- build/Build.cs
+++ build/Build.cs
@@ -1,156 +1,145 @@
-namespace ProvisionData.Build
+using Nuke.Common;
+using Nuke.Common.Execution;
+using Nuke.Common.Git;
+using Nuke.Common.IO;
+using Nuke.Common.ProjectModel;
+using Nuke.Common.Tooling;
+using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Tools.GitVersion;
+using Nuke.Common.Utilities.Collections;
+using System;
+using System.Linq;
+using static Nuke.Common.IO.FileSystemTasks;
+using static Nuke.Common.IO.PathConstruction;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
+
+[CheckBuildProjectConfigurations]
+[UnsetVisualStudioEnvironmentVariables]
+[DotNetVerbosityMapping]
+class Build : NukeBuild
 {
-    using Nuke.Common;
-    using Nuke.Common.Execution;
-    using Nuke.Common.Git;
-    using Nuke.Common.IO;
-    using Nuke.Common.ProjectModel;
-    using Nuke.Common.Tooling;
-    using Nuke.Common.Tools.DotNet;
-    using Nuke.Common.Tools.GitVersion;
-    using Nuke.Common.Utilities.Collections;
-    using System;
-    using System.Linq;
-    using static Nuke.Common.IO.FileSystemTasks;
-    using static Nuke.Common.IO.PathConstruction;
-    using static Nuke.Common.Tools.DotNet.DotNetTasks;
-    using static Nuke.GitHub.ChangeLogExtensions;
+	/// Support plugins are available for:
+	///   - JetBrains ReSharper        https://nuke.build/resharper
+	///   - JetBrains Rider            https://nuke.build/rider
+	///   - Microsoft VisualStudio     https://nuke.build/visualstudio
+	///   - Microsoft VSCode           https://nuke.build/vscode
 
-    [CheckBuildProjectConfigurations]
-    [UnsetVisualStudioEnvironmentVariables]
-    class Build : NukeBuild
-    {
-        // Support plug-ins are available for:
-        //   - JetBrains ReSharper        https://nuke.build/resharper
-        //   - JetBrains Rider            https://nuke.build/rider
-        //   - Microsoft VisualStudio     https://nuke.build/visualstudio
-        //   - Microsoft VSCode           https://nuke.build/vscode
+	public static int Main() => Execute<Build>(x => x.Compile);
 
-        public static Int32 Main() => Execute<Build>(x => x.Compile);
+	[Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
+	readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+	[Parameter("NuGet server URL.")]
+	readonly String NugetSource = "https://api.nuget.org/v3/index.json";
+	[Parameter("NuGet API Key.")]
+	readonly String NugetApiKey;
+	[Parameter("NuGet package version.")]
+	readonly String PackageVersion;
 
-        [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
-        readonly Configuration Configuration = Configuration.Release;
+	[Solution(GenerateProjects = true)] readonly Solution Solution;
+	[GitRepository] readonly GitRepository GitRepository;
+	[GitVersion(Framework = "net5.0")] readonly GitVersion GitVersion;
 
-        [Parameter("Explicit framework to build")] readonly String Framework;
-        [Parameter("NuGet API Key")] readonly String NuGetApiKey;
-        [Parameter("PDSI API Key")] readonly String PdsiApiKey;
+	AbsolutePath SourceDirectory => RootDirectory / "source";
+	AbsolutePath TestsDirectory => RootDirectory / "tests";
+	AbsolutePath OutputDirectory => RootDirectory / "output";
 
-        [Solution] readonly Solution Solution;
-        [GitRepository] readonly GitRepository GitRepository;
-        [GitVersion(Framework = "netcoreapp3.1")]
-        readonly GitVersion GitVersion;
 
-        AbsolutePath SourceDirectory => RootDirectory / "source";
-        AbsolutePath TestsDirectory => RootDirectory / "tests";
-        AbsolutePath ArtifactsDirectory => RootDirectory / "artifacts";
-        String ChangeLogFile => RootDirectory / "CHANGELOG.md";
+	Project Common => Solution.GetProject("ProvisionData.Common");
+	Project EFCore => Solution.GetProject("ProvisionData.EntityFrameworkCore");
+	Project GELF => Solution.GetProject("ProvisionData.GELF");
+	Project HTML => Solution.GetProject("ProvisionData.Html");
+	Project Serilog => Solution.GetProject("ProvisionData.Serilog");
+	Project Specs => Solution.GetProject("ProvisionData.Specifications");
+	Project SpecsEFCore => Solution.GetProject("ProvisionData.Specifications.EntityFrameworkCore");
+	Project Testing => Solution.GetProject("ProvisionData.UnitTesting");
 
-        Target Clean => _ => _
-            .Executes(() =>
-            {
-                SourceDirectory.GlobDirectories("**/bin", "**/obj").ForEach(DeleteDirectory);
-                TestsDirectory.GlobDirectories("**/bin", "**/obj").ForEach(DeleteDirectory);
-                EnsureCleanDirectory(ArtifactsDirectory);
-            });
+	Project[] Projects => new[] { Common, EFCore, GELF, HTML, Serilog, Specs, SpecsEFCore, Testing };
 
-        Target Restore => _ => _
-            .DependsOn(Clean)
-            .Executes(() =>
-            {
-                DotNetRestore(_ => _
-                    .SetProjectFile(Solution));
-            });
+	Target Clean => _ => _
+		.Before(Restore)
+		.Executes(() =>
+		{
+			SourceDirectory.GlobDirectories("**/bin", "**/obj").ForEach(DeleteDirectory);
+			TestsDirectory.GlobDirectories("**/bin", "**/obj").ForEach(DeleteDirectory);
+			EnsureCleanDirectory(OutputDirectory);
+		});
 
-        Target Compile => _ => _
-            .DependsOn(Restore)
-            .Executes(() =>
-            {
-                DotNetBuild(_ => _
-                    .SetProjectFile(Solution)
-                    .SetConfiguration(Configuration)
-                    .EnableNoRestore());
-            });
+	Target Restore => _ => _
+		.Executes(() =>
+		{
+			DotNetRestore(s => s
+				.SetProjectFile(Solution)
+			);
+		});
 
-        Target Publish => _ => _
-            .DependsOn(Compile)
-            .Executes(() =>
-            {
-                var publishCombinations = from project in Solution.Projects
-                                          from framework in project.GetTargetFrameworks()
-                                          select new { project, framework };
+	Target Compile => _ => _
+		.DependsOn(Restore)
+		.Executes(() =>
+		{
+			DotNetBuild(s => s
+				.EnableNoRestore()
+				.SetProjectFile(Solution)
+				.SetConfiguration(Configuration)
+				.SetAssemblyVersion(GitVersion.AssemblySemVer)
+				.SetFileVersion(GitVersion.AssemblySemFileVer)
+				.SetInformationalVersion(GitVersion.InformationalVersion)
+			);
 
-                DotNetPublish(_ => _
-                    .EnableNoRestore()
-                    .SetConfiguration(Configuration)
-                    .SetAssemblyVersion(GitVersion.AssemblySemVer)
-                    .SetFileVersion(GitVersion.AssemblySemFileVer)
-                    .SetInformationalVersion(GitVersion.InformationalVersion)
-                    .CombineWith(publishCombinations, (oo, v) => oo
-                        .SetProject(v.project)
-                        .SetFramework(v.framework)
-                        )
-                    );
-            });
+			DotNetPublish(s => s
+				.EnableNoRestore()
+				.EnableNoBuild()
+				.SetConfiguration(Configuration)
+				.SetAssemblyVersion(GitVersion.AssemblySemVer)
+				.SetFileVersion(GitVersion.AssemblySemFileVer)
+				.SetInformationalVersion(GitVersion.InformationalVersion)
+				.CombineWith(
+					from project in Projects
+					from framework in project.GetTargetFrameworks()
+					select new { project, framework }, (cs, v) => cs
+						.SetProject(v.project)
+						.SetFramework(v.framework)
+				)
+			);
+		});
 
-        Target Test => _ => _
-                   .DependsOn(Publish)
-                   .Executes(() =>
-                   {
-                       DotNetTest(s => s
-                           .SetConfiguration(Configuration)
-                           .EnableNoBuild()
-                           .EnableNoRestore()
-                           .SetLogger("trx")
-                           .SetLogOutput(true)
-                           .SetArgumentConfigurator(arguments => arguments.Add("/p:UseSourceLink={0}", "true"))
-                           .SetResultsDirectory(TestsDirectory / "results"));
-                   });
+	Target Test => _ => _
+		.DependsOn(Compile)
+		.Executes(() =>
+		{
+			DotNetTest(s => s
+				.SetProjectFile(Solution)
+				.SetConfiguration(Configuration)
+				.EnableNoRestore()
+				.EnableNoBuild()
+			);
+		});
 
-        Target Pack => _ => _
-            .DependsOn(Test)
-            .Executes(() =>
-            {
-                var changeLog = GetCompleteChangeLog(ChangeLogFile)
-                                    .EscapeStringPropertyForMsBuild();
+	Target Pack => _ => _
+		.DependsOn(Clean, Test)
+		.Requires(() => Configuration == Configuration.Release)
+		.Executes(() =>
+		{
+			DotNetPack(s => s
+				.EnableNoRestore()
+				.EnableNoBuild()
+				.SetConfiguration(Configuration)
+				.SetOutputDirectory(OutputDirectory)
+				.CombineWith(Projects, (s, p) => s
+					.SetProject(p)
+					.SetProperty("PackageVersion", PackageVersion ?? GitVersion.NuGetVersionV2))
 
-                DotNetPack(s => s
-                     .SetConfiguration(Configuration)
-                     .EnableIncludeSymbols()
-                     .EnableNoBuild()
-                     .EnableNoRestore()
-                     .SetProperty("PackageVersion", GitVersion.NuGetVersionV2)
-                     .SetOutputDirectory(ArtifactsDirectory / "nuget")
-                     .SetPackageReleaseNotes(changeLog));
-            });
+			);
+		});
 
-        Target PushToNuGet => _ => _
-            .DependsOn(Pack)
-            .Requires(() => NuGetApiKey)
-            .Requires(() => Equals(Configuration, Configuration.Release))
-            .Executes(() =>
-            {
-                GlobFiles(ArtifactsDirectory / "nuget", "*.nupkg")
-                    .NotEmpty()
-                    .Where(x => !x.EndsWith(".symbols.nupkg"))
-                    .ForEach(x => DotNetNuGetPush(s => s
-                        .SetTargetPath(x)
-                        .SetSource("https://api.nuget.org/v3/index.json")
-                        .SetApiKey(NuGetApiKey))
-                    );
-            });
-
-        Target PushToPdsi => _ => _
-            .DependsOn(Pack)
-            .Requires(() => PdsiApiKey)
-            .Requires(() => Equals(Configuration, Configuration.Release))
-            .Executes(() =>
-            {
-                GlobFiles(ArtifactsDirectory / "nuget", "*.nupkg")
-                    .NotEmpty()
-                    .ForEach(x => DotNetNuGetPush(s => s
-                        .SetTargetPath(x)
-                        .SetSource("https://baget.pdsint.net/v3/index.json")
-                        .SetApiKey(PdsiApiKey)));
-            });
-    }
+	Target Push => _ => _
+		.Executes(() =>
+		{
+			DotNetNuGetPush(s => s
+				.SetSource(NugetSource)
+				.SetApiKey(NugetApiKey)
+				.CombineWith(OutputDirectory.GlobFiles("*.nupkg"), (s, v) => s
+					.SetTargetPath(v)
+				)
+			);
+		});
 }
