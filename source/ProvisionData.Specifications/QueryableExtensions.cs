71cfc8ca9d603bf7e38012f5faf358243b1b092f
Doug Wilson <d@dkw.io>
2021-10-05 10:04:01 -0700

    Added exception handling

--- source/ProvisionData.Specifications/QueryableExtensions.cs
+++ source/ProvisionData.Specifications/QueryableExtensions.cs
@@ -2,23 +2,23 @@
  * MIT License
  *
  * Copyright 2021 Provision Data Systems Inc.  https://provisiondata.com
- * 
- * Permission is hereby granted, free of charge, to any person obtaining a 
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
  * to deal in the Software without restriction, including without limitation
  * the rights to use, copy, modify, merge, publish, distribute, sub-license,
- * and/or sell copies of the Software, and to permit persons to whom the 
+ * and/or sell copies of the Software, and to permit persons to whom the
  * Software is furnished to do so, subject to the following conditions:
  *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
  * DEALINGS IN THE SOFTWARE.
  *
  *******************************************************************************/
@@ -43,15 +43,32 @@ namespace ProvisionData.Specifications
 			Int32 count = 0;
 			foreach (var item in sortModels)
 			{
-				var parameter = Expression.Parameter(typeof(T), "x");
-				var selector = Expression.PropertyOrField(parameter, item.PropertyName);
-				var method = item.Descending ?
-					(count == 0 ? OrderByDesc : ThenByDesc) :
-					(count == 0 ? OrderBy : ThenBy);
-				expression = Expression.Call(typeof(Queryable), method, 
-					new Type[] { source.ElementType, selector.Type }, expression, 
-					Expression.Quote(Expression.Lambda(selector, parameter)));
-				count++;
+				try
+				{
+					if (String.IsNullOrWhiteSpace(item.PropertyName))
+						continue;
+
+					var parameter = Expression.Parameter(typeof(T), "x");
+					var selector = Expression.PropertyOrField(parameter, item.PropertyName);
+					var method = item.Descending ?
+						(count == 0 ? OrderByDesc : ThenByDesc) :
+						(count == 0 ? OrderBy : ThenBy);
+					expression = Expression.Call(typeof(Queryable), method,
+						new Type[] { source.ElementType, selector.Type }, expression,
+						Expression.Quote(Expression.Lambda(selector, parameter)));
+					count++;
+				}
+				catch (ArgumentException)
+				{
+					// Likely thrown by the call to Expression.PropertyOrField(parameter, item.PropertyName)
+					// ArgumentNullException => expression or propertyOrFieldName is null.
+					// ArgumentException => No property or field named propertyOrFieldName is defined in
+					// expression.Type or its base types.
+
+					// Either way, it's safe to ignore. Hopefully the developer will realize their mistake
+					// but lets not make life hard for them by throwing an exception from this deep in
+					// their code.
+				}
 			}
 			return count > 0 ? source.Provider.CreateQuery<T>(expression) : source;
 		}
